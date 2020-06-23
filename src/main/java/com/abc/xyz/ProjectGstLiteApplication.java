d3ed044a835f529ef0fd66983970b0b2fbf68a39
Pham Phu Thinh <thinhpv7@gmail.com>
2020-06-23 20:15:50 +0700

    Init project GST Lite BackEnd

---
+++ src/main/java/com/abc/xyz/ProjectGstLiteApplication.java
@@ -0,0 +1,13 @@
+package com.abc.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGstLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGstLiteApplication.class, args);
+	}
+
+}
